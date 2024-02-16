4a6f275b3440de245e28aa21bbe82403b637fca2
badri119 <badrinathv98@gmail.com>
2024-02-15 17:44:20 -0800

    testing deployment with postgres DB

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ const getAllQuotes = require("./routes/quotes");
 const httpCodes = require("./routes/http_codes");
 const example = require("./routes/example");
 
-// app.use("/quotes", getAllQuotes);
-// app.use("/http", httpCodes);
-app.use("/example", example);
+app.use("/quotes", getAllQuotes);
+app.use("/http", httpCodes);
+// app.use("/example", example);
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`);
