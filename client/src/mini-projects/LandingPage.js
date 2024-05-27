ff559539fa512ff901fb8ef1aa200a0adea42433
badri119 <badrinathv98@gmail.com>
2024-05-27 14:28:07 -0700

    moved the big cool button to a component

--- client/src/mini-projects/LandingPage.js
+++ client/src/mini-projects/LandingPage.js
@@ -6,6 +6,7 @@ import movie from "../images/movies.png";
 import cart from "../images/cart.png";
 import project from "../images/project.png";
 import "./LandingPage.css";
+import MainButton from "./components/MainButton";
 
 const LandingPage = () => {
   const apis = [
@@ -110,42 +111,7 @@ const LandingPage = () => {
                     to={urlto}
                     className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
                   >
-                    <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-                    <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                      <svg
-                        className="w-5 h-5 text-green-400"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M14 5l7 7m0 0l-7 7m7-7H3"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                      <svg
-                        className="w-5 h-5 text-green-400"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M14 5l7 7m0 0l-7 7m7-7H3"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                      {button}
-                    </span>
+                    <MainButton button={button} />
                   </Link>
                 </div>
               </div>
